7ddd972f5754c6ed84ad083417f076920713e0d6
Kiana <s.kiana.hosseini@gmail.com>
2019-05-29 22:40:45 -0700

    3 classifier tests

---
+++ choose_your_own/random_forest.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+import matplotlib.pyplot as plt
+from prep_terrain_data import makeTerrainData
+from class_vis import prettyPicture
+
+features_train, labels_train, features_test, labels_test = makeTerrainData()
+
+
+### the training data (features_train, labels_train) have both "fast" and "slow" points mixed
+### in together--separate them so we can give them different colors in the scatterplot,
+### and visually identify them
+grade_fast = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==0]
+bumpy_fast = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==0]
+grade_slow = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==1]
+bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==1]
+
+
+#### initial visualization
+plt.xlim(0.0, 1.0)
+plt.ylim(0.0, 1.0)
+plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
+plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
+plt.legend()
+plt.xlabel("bumpiness")
+plt.ylabel("grade")
+plt.show()
+#################################################################################
+
+##Random Forest
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score as accur
+
+clf = RandomForestClassifier(n_estimators=100, max_depth=2, random_state=0)
+clf.fit(features_train, labels_train)  
+pred = clf.predict(features_test)
+acc = accur(pred, labels_test)
+
+print(acc)
+
+try:
+    prettyPicture(clf, features_test, labels_test)
+except NameError:
+    pass
